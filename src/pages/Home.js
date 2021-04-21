98dd4a78f8d1f482c6ed061f3f736e4ac71e667f
Austin Ruhf <austin.ruhf@gmail.com>
2021-04-21 17:14:06 -0400

    fixed front page display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,16 @@ function Home() {
       <Row>
         <Col>
           <Section>
-            <div className="col-sm-12 col-lg-6">
-                <img src={Austin} className="img-fluid self" alt="Austin"/>
-            </div>
-            <div className="col-sm-12 col-lg-6">
-                <p> 
-                Virginia born and raised, I've been a lifeguard, landscaper, paddleboat merchant, prep cook, and hazardous waste technician. I've been in a dozen or so bands as a guitarist, bassist, drummer, and/or singer, including my current self-led 4-piece called Ulmeyda. Right now I'm building and decorating cakes at local Richmond bakery Shyndigz and I'm learning web development to launch a new career while stuck in quarantine. Programming is new to me but I love to solve problems and create new things.
-                </p>
-            </div> 
+            <Row>
+              <div className="col-sm-12 col-lg-6">
+                  <img src={Austin} className="img-fluid self" alt="Austin"/>
+              </div>
+              <div className="col-sm-12 col-lg-6">
+                  <p> 
+                  Virginia born and raised, I've been a lifeguard, landscaper, paddleboat merchant, prep cook, and hazardous waste technician. I've been in a dozen or so bands as a guitarist, bassist, drummer, and/or singer, including my current self-led 4-piece called Ulmeyda. Right now I'm building and decorating cakes at local Richmond bakery Shyndigz and I'm learning web development to launch a new career while stuck in quarantine. Programming is new to me but I love to solve problems and create new things.
+                  </p>
+              </div>
+            </Row>
           </Section>
         </Col>
       </Row>
