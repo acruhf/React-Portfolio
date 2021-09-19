4ce45c503eeca7faaa4e34deb71975b81ad763de
Austin Ruhf <austin.ruhf@gmail.com>
2021-09-19 09:59:53 -0400

    edited home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
               </div>
               <div className="col-sm-12 col-lg-6">
                   <p> 
-                  Virginia born and raised, I've been a lifeguard, landscaper, paddleboat merchant, prep cook, and hazardous waste technician. I've been in a dozen or so bands as a guitarist, bassist, drummer, and/or singer, including my current self-led 4-piece called Ulmeyda. Right now I'm building and decorating cakes at local Richmond bakery Shyndigz and I'm learning web development to launch a new career while stuck in quarantine. Programming is new to me but I love to solve problems and create new things.
+                  Virginia born and raised, I've been a lifeguard, landscaper, paddleboat merchant, prep cook, and hazardous waste technician. I've been playing music my whole life as a cellist, guitarist, bassist, and drummer. Right now I'm building and decorating cakes at local Richmond bakery Shyndigz and I'm learning web development to launch a new career. Programming is new to me but I love to solve problems and create new things.
                   </p>
               </div>
             </Row>
